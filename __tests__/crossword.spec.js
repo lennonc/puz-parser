dcf0c2fb8e4161bb58548575e18e60c635e7d3f5
Lennon Chimbumu <lennonc@users.noreply.github.com>
2019-03-05 22:58:06 +0200

    Parse the header of the `.puz` file
    Also add some actual tests

--- __tests__/crossword.spec.js
+++ __tests__/crossword.spec.js
@@ -1,5 +1,13 @@
+const Crossword = require('../lib/crossword');
+
+const fileName = `${__dirname}/fixtures/classic.puz`;
+const fileOpts = { fileName };
+
 describe('Crossword', () => {
-  it('should pass', () => {
-    expect(1).toBe(1);
+  it('should parse the file', () => {
+    const crossword = new Crossword(fileOpts);
+    const json = crossword.toJson();
+    expect(json).toHaveProperty('header');
+    expect(json.header).not.toBeUndefined();
   });
 });
